529ba242105ccf58d9e4e3b7066806708f5da08c
theirix <theirix@gmail.com>
2023-05-08 22:03:30 +0300

    CI: wait for Postgres to start

--- docker/test.sh
+++ docker/test.sh
@@ -3,6 +3,13 @@ set -eux
 cp -av /workspace/* .
 make
 sudo make install
+
+if command pg_isready ; then
+  timeout 600 bash -c 'until pg_isready; do sleep 10; done'
+else
+  sleep 30
+fi
+
 set +e
 if sudo -u postgres make installcheck ; then
   echo Success
